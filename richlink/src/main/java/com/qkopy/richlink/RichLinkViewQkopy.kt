786d2919cc1010e03ae6b7be545777c35a42c4dc
ajithvgiri <ajithvgiri@qkopy.com>
2019-08-28 12:16:12 +0530

    crash fix and added chrome custom tabs commit

--- richlink/src/main/java/com/qkopy/richlink/RichLinkViewQkopy.kt
+++ richlink/src/main/java/com/qkopy/richlink/RichLinkViewQkopy.kt
@@ -6,23 +6,30 @@ import android.content.Intent
 import android.net.Uri
 import android.util.AttributeSet
 import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.browser.customtabs.CustomTabsIntent
-import androidx.core.content.res.ResourcesCompat
 import com.bumptech.glide.Glide
 import com.qkopy.richlink.data.database.MetaDatabase
 import com.qkopy.richlink.data.model.MetaData
-import kotlinx.android.synthetic.main.qkopy_link_layout.view.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
+
 open class RichLinkViewQkopy : RelativeLayout {
+
 
     private var view: View?=null
     var metaData: MetaData? = null
     internal var context: Context
 
+
+    private lateinit var linearLayout: LinearLayout
+    private lateinit var imageView: ImageView
+    private lateinit var tvTitle: TextView
+    private lateinit var tvDescription: TextView
 
     private var mainUrl: String? = null
 
@@ -46,6 +53,8 @@ open class RichLinkViewQkopy : RelativeLayout {
 
     fun initView() {
 
+
+
         if (findLinearLayoutChild() != null) {
             this.view = findLinearLayoutChild()
         } else {
@@ -54,18 +63,25 @@ open class RichLinkViewQkopy : RelativeLayout {
         }
 
 
+        linearLayout = findViewById<View>(R.id.linearLayout) as LinearLayout
+        imageView = findViewById<View>(R.id.imageViewBanner) as ImageView
+        tvTitle = findViewById<View>(R.id.textViewTitle) as TextView
+        tvDescription = findViewById<View>(R.id.textViewDescription) as TextView
 
-        imageViewBanner.visibility = View.VISIBLE
-        Glide.with(context).load(metaData?.image).placeholder(R.drawable.notfound).error(R.drawable.notfound).into(imageViewBanner)
-        textViewTitle.text = metaData?.title
+
+
+
+        imageView.visibility = View.VISIBLE
+        Glide.with(context).load(metaData?.image).placeholder(R.drawable.notfound).error(R.drawable.notfound).into(imageView)
+        tvTitle.text = metaData?.title
 
         if (metaData?.description?.isEmpty() == true || metaData?.description == "") {
-            textViewDescription.visibility = View.GONE
+            tvDescription.visibility = View.GONE
         } else {
-            textViewDescription.visibility = View.VISIBLE
-            textViewDescription.text = metaData!!.description
+            tvDescription.visibility = View.VISIBLE
+            tvDescription.text = metaData?.description
         }
-        textViewDescription.text = metaData?.url ?: mainUrl
+        tvDescription.text = metaData?.url ?: mainUrl
 
 
         linearLayout.setOnClickListener { view ->
@@ -84,18 +100,23 @@ open class RichLinkViewQkopy : RelativeLayout {
 
 
     private fun richLinkClicked() {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(mainUrl))
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        context.startActivity(intent)
-        // CustomTabsIntent.Builder used to configure CustomTabsIntent.
-        val builder = CustomTabsIntent.Builder().setShowTitle(true)
-        builder.setToolbarColor(ResourcesCompat.getColor(resources, R.color.blue_grey_300, null))
-        // For adding menu item
-        builder.addMenuItem("Share", getItem())
-        builder.addDefaultShareMenuItem()
-        // CustomTabsIntent used to launch the URL
+//        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(mainUrl))
+//        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//        context.startActivity(intent)
+//        // CustomTabsIntent.Builder used to configure CustomTabsIntent.
+//        val builder = CustomTabsIntent.Builder().setShowTitle(true)
+//        builder.setToolbarColor(ResourcesCompat.getColor(resources, R.color.blue_grey_300, null))
+//        // For adding menu item
+//        builder.addMenuItem("Share", getItem())
+//        builder.addDefaultShareMenuItem()
+//        // CustomTabsIntent used to launch the URL
+//        val customTabsIntent = builder.build()
+//        // Open the Custom Tab
+//        customTabsIntent.launchUrl(context, Uri.parse(mainUrl))
+
+
+        val builder = CustomTabsIntent.Builder()
         val customTabsIntent = builder.build()
-        // Open the Custom Tab
         customTabsIntent.launchUrl(context, Uri.parse(mainUrl))
     }
 
@@ -112,6 +133,20 @@ open class RichLinkViewQkopy : RelativeLayout {
         return if (childCount > 0 && getChildAt(0) is LinearLayout) {
             getChildAt(0) as LinearLayout
         } else null
+    }
+
+    fun setLinkFromMeta(metaData: MetaData) {
+        this.metaData = metaData
+//        initView()
+    }
+
+
+    fun setDefaultClickListener(isDefault: Boolean) {
+        isDefaultClick = isDefault
+    }
+
+    fun setClickListener(richLinkListener1: RichLinkListener) {
+        richLinkListener = richLinkListener1
     }
 
     fun setLink(url: String, context: Context, viewListener: ViewListener) {
